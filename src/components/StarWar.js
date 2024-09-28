7f8c7d40ed42a3aa1c1ece70e59474c7beb71039
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-09-28 01:11:31 +0100

    bst_consulting assessment

--- src/components/StarWar.js
+++ src/components/StarWar.js
@@ -37,7 +37,7 @@ function StarWars() {
           <p className="text-white text-[10px] mt-[0.5rem]">Fronted Assessment Test for Ekundayo Mayowa T.</p>
         </nav>
         <div className="p-4 flex justify-center">
-          <div className="md:w-[30rem]">
+          <div className="sm:w-[30rem]">
             <input
               type="text"
               placeholder="Search Star Wars characters..."
